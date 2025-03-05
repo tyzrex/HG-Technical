7d89afe5e86090cabf67f8c5b3db7d39c8ff4f1f
tyzrex <sulavbaral58@gmail.com>
2025-03-06 00:41:41 +0545

    remove unused font import from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Outfit, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/global/footer";
 import Header from "@/components/global/header";
